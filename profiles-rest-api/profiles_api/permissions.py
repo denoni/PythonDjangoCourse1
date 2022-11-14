6080b6227349cf214e1acdbeee757d01a0dc074f
gabrieldenoni <gabrieldenonifake@gmail.com>
2022-11-14 10:53:15 -0300

    Added user profile feed API

--- profiles-rest-api/profiles_api/permissions.py
+++ profiles-rest-api/profiles_api/permissions.py
@@ -10,3 +10,14 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
         
         return obj.id == request.user.id
+    
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allows users to update their own status"""
+    
+    def has_object_permission(self, request, view, obj):
+        """Check if the user is trying to update their own profile"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.user_profile.id == request.user.id
--- profiles-rest-api/profiles_api/serializers.py
+++ profiles-rest-api/profiles_api/serializers.py
@@ -34,3 +34,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
         if 'password' in validated_data:
             password = validated_data.pop('password')
             instance.set_password(password)
+            
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+    
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
