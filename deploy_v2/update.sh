7be237e82a7a8b58bda61c7eb3cc97def68c6c7f
gabrieldenoni <gabrieldenonifake@gmail.com>
2022-11-14 13:44:19 -0300

    return to previous path

--- deploy_v2/update.sh
+++ deploy_v2/update.sh
@@ -5,8 +5,8 @@ set -e
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 git pull
-$PROJECT_BASE_PATH/env/bin/python profiles-rest-api/manage.py migrate
-$PROJECT_BASE_PATH/env/bin/python profiles-rest-api/manage.py collectstatic --noinput
+$PROJECT_BASE_PATH/env/bin/python manage.py migrate
+$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
 supervisorctl restart profiles_api
 
 echo "DONE! :)"
