38b0c2381e324c5f2b40733022e22ba31f8ede98
gabrieldenoni <gabrieldenonifake@gmail.com>
2022-11-14 13:07:47 -0300

    Configured for deployment to AWS

--- profiles-rest-api/profiles_project/settings.py
+++ profiles-rest-api/profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*o0u*g)%2m*bz)(000l41fy55_moep!3m@8=e=0=4wjlth291z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
